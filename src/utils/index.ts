25e7372fc7911388f2c88ae807a8772134c2ba61
Adrian Hideki <adrian.hideki.br@gmail.com>
2025-07-15 16:52:03 -0300

    :wrench: chore: exporting theme utils functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,24 @@
+import {
+  generateColorScale,
+  getContrastColor,
+  getIsDarkMode,
+  injectColorCss,
+  injectFontCss,
+  injectSpacingCss,
+  transformTheme,
+} from 'theme-token-manager'
+
 import { getTransparencyColor } from './getTransparencyColor'
 import { renderTextWithLineBreak } from './renderTextWithLineBreak'
 
-export { getTransparencyColor, renderTextWithLineBreak }
+export {
+  generateColorScale,
+  getContrastColor,
+  getIsDarkMode,
+  getTransparencyColor,
+  injectColorCss,
+  injectFontCss,
+  injectSpacingCss,
+  renderTextWithLineBreak,
+  transformTheme,
+}
